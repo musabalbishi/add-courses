6a8d646190d82584423087baf4bcc3a2999168cf
Mosab albishi <90303281+musabalbishi@users.noreply.github.com>
2023-05-06 22:04:22 +0300

    problem solved. switched the server to local

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const parser = require("body-parser");
 const path = require("path");
+
 mongoose
-  .connect(
-    "mongodb+srv://meshal:123456789m@cluster0.bwfbl6v.mongodb.net/?retryWrites=true&w=majority"
-  )
+  .connect("mongodb://0.0.0.0:27017/twuiq", {})
   .then(() => {
     console.log("connected");
   })
@@ -13,16 +12,31 @@ mongoose
     console.log(error);
   });
 //
-const User = require("./models/User");
 const app = express();
 app.set("view engine", "ejs");
 app.use(parser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+const User = require("./models/User");
 
+// const create = new User({
+//   course: "course",
+//   desc: "desc",
+//   student: 33,
+// });
+// //
+// create
+//   .save()
+//   .then(() => {
+//     console.log("record inserted");
+//     // res.redirect("/home");
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
 //
 
 //
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
   res.render("home.ejs");
 });
 app.post("/courses", (req, res) => {
@@ -39,7 +53,7 @@ app.post("/courses", (req, res) => {
     .save()
     .then(() => {
       console.log("record inserted");
-      res.redirect("/home");
+      res.redirect("/");
     })
     .catch((error) => {
       console.log(error);
@@ -48,9 +62,15 @@ app.post("/courses", (req, res) => {
 //
 app.get("/courses", async (req, res) => {
   // retrive data from mongodb collection
-  //   const results = await User.find();
+  const results = await User.find();
   res.render("courses.ejs", { all: results });
 });
+//
+// app.post("/courses", async (req, res) => {
+//   let course = req.body.search;
+//   const results = await User.find();
+//   res.render("courses.ejs", { query: results });
+// });
 //
 app.listen(8000, () => {
   console.log("listening ...");
