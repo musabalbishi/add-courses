cd95519231d6a4fdb1c60dff117c6776c358940a
Mosab albishi <90303281+musabalbishi@users.noreply.github.com>
2023-05-07 15:03:09 +0300

    todays lab is included(w2-06-lab)

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const parser = require("body-parser");
 const path = require("path");
 
 mongoose
-  .connect("mongodb://0.0.0.0:27017/twuiq", {})
+  .connect(
+    // "mongodb+srv://mosabalbishi:musab123m@cluster0.xsllfeu.mongodb.net/?retryWrites=true&w=majority",
+    // { AUTHENTICATION_DATABASE: null }
+    "mongodb://0.0.0.0:27017/twuiq"
+  )
   .then(() => {
     console.log("connected");
   })
@@ -17,6 +21,7 @@ app.set("view engine", "ejs");
 app.use(parser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 const User = require("./models/User");
+const { off } = require("process");
 
 //
 app.get("/", (req, res) => {
@@ -44,17 +49,52 @@ app.post("/courses", (req, res) => {
 });
 //
 app.get("/courses", async (req, res) => {
-  // retrive data from mongodb collection
   const results = await User.find();
   res.render("courses.ejs", { all: results });
 });
+
 //
-app.post("/courses", async (req, res) => {
-  let title = req.body.search;
-  const results = await User.find({ course: title });
-  res.render("courses.ejs", { query: results });
+// app.get("/details", async (req, res) => {
+//   const results = await User.find();
+//   for (let i = 0; i < results.length; i++) {
+//     res.render("details.ejs", { details: results[i] });
+//   }
+// });
+// READ
+app.get("/courses/:cname", (req, res) => {
+  const cname = req.params.cname;
+  User.find({ course: cname })
+    .then((cname) => {
+      res.render("courses.ejs", { all: cname });
+    })
+    .catch((e) => {
+      res.send(e.message);
+    });
 });
+
+// DELETE
+app.get("/deleteCourses", (req, res) => {
+  const courseName = req.query.course;
+  User.deleteOne({ course: courseName }).then(() => {
+    res.send(`${courseName} has been deleted`);
+  });
+});
+// UPDATE --> use querey parameter
+app.get("/updateCourse/:cname", (req, res) => {
+  const newDescription = req.query.newDesc;
+  User.findById(req.params.cname).then((c) => {
+    c.desc = newDescription;
+    c.save()
+      .then(() => {
+        res.send("updated");
+      })
+      .catch((e) => {
+        res.send(e.message);
+      });
+  });
+});
+
 //
-app.listen(8000, () => {
+app.listen(8800, () => {
   console.log("listening ...");
 });
